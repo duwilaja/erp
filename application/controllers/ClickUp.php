218cfa49378f9b3f7d01f9c606faeb6181cd1ac4
duwilaja <apaaja.software@gmail.com>
2023-11-27 09:02:05 +0700

    klikap

--- application/controllers/ClickUp.php
+++ application/controllers/ClickUp.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class ClickUp extends CI_Controller {
     
     public function __construct()
-    {
+    { 
         parent::__construct();
 	}
 	
